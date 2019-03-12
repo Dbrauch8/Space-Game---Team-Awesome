72024bf57f4c7fc4d962abe1f5eb35c9d2fc6911
Doug Brauch <doug.c.brauch@outlook.com>
2019-03-12 14:20:51 -0400

    Defining Ship Classes (Working on Ship 1)

---
+++ Space Game/ShipOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//using Xamarin.Forms;
+
+namespace Space_Game
+{
+    public class ShipOne  
+    {
+       public void FuelLevel()
+        {
+            //Determine unit of measure
+            //tied to speed, need a formula for fuel consumption related to speed
+        }
+        public void Speed()
+        {
+            //tied to fuel level, as speed is increased n% as fuel level decreases/tied to time. 
+
+        }
+        public void CargoLevel()
+        {
+            //Placeholder Array / List of inventory items. 
+            //Maximum quantity level (space and weight)
+        }
+
+    }
+}

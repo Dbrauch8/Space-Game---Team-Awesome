ce56a4ac695ff02a8c625b3722001c800987bd4e
Doug Brauch <doug.c.brauch@outlook.com>
2019-03-13 07:33:55 -0400

    One class per ship

--- Space Game/ShipOne.cs
+++ Space Game/ShipOne.cs
@@ -31,4 +31,52 @@ namespace Space_Game
         }
 
     }
+    public class ShipTwo
+    {
+        public void FuelLevel()
+        {
+            //Determine unit of measure
+            //tied to speed, need a formula for fuel consumption related to speed
+        }
+        public void Speed()
+        {
+            //tied to fuel level, as speed is increased n% as fuel level decreases/tied to time. 
+            // Warp Speed Formula: 
+
+            var w;
+            double v;
+
+            v(W) = W(10 / 3) + Math.Pow(10.0 - 5.0, (3.67)); //Original formula => v(W) = (-11 / 3)= -3.67;
+        }
+        public void CargoLevel()
+        {
+            //Placeholder Array / List of inventory items. 
+            //Maximum quantity level (space and weight)
+        }
+
+    }
+    public class ShipThree
+    {
+        public void FuelLevel()
+        {
+            //Determine unit of measure
+            //tied to speed, need a formula for fuel consumption related to speed
+        }
+        public void Speed()
+        {
+            //tied to fuel level, as speed is increased n% as fuel level decreases/tied to time. 
+            // Warp Speed Formula: 
+
+            var w;
+            double v;
+
+            v(W) = W(10 / 3) + Math.Pow(10.0 - 5.0, (3.67)); //Original formula => v(W) = (-11 / 3)= -3.67;
+        }
+        public void CargoLevel()
+        {
+            //Placeholder Array / List of inventory items. 
+            //Maximum quantity level (space and weight)
+        }
+
+    }
 }
